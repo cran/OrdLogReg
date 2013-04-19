c6feb54ffe21d09519926811efbf5ea1e833eb4f
Bethany Wolf <wolfb@musc.edu>
2013-04-19 00:00:00 +0000

    version 1.0

---
+++ R/plot.Ord.logreg.R
@@ -0,0 +1,54 @@
+plot.Ord.logreg <-
+function(x, one.plot=TRUE,...)
+{
+ if (class(x)!="Ord.logreg")
+stop("x is not of class Ord.logreg")
+ model<-x$model
+ cat<-length(model)
+ if (one.plot==TRUE)
+  {
+  if(cat<4)
+   {
+   par(mfrow=c(1,3))
+   for (i in 1:cat)
+     {
+     plot(model[[i]], title=FALSE)
+     mtext(paste("Category", i, "Model"), side=3, line=1)
+     }
+   }
+  if(cat==4)
+   {
+   par(mfrow=c(2,2))
+   for (i in 1:cat)
+     {
+     plot(model[[i]], title=FALSE)
+     mtext(paste("Category", i, "Model"), side=3, line=1)
+     }
+   }
+  if(cat>4)
+   {
+   par(mfrow=c(2,2))
+   for (i in 1:4)
+     {
+     plot(model[[i]], title=FALSE)
+     mtext(paste("Category", i, "Model"), side=3, line=1)
+     }
+    x11()
+    par(mfrow=c(2,2))
+    for(j in 5:cat)
+     {
+     plot(model[[j]], title=FALSE)
+     mtext(paste("Category", j, "Model"), side=3, line=1)
+     } 
+   }
+ }
+ if (one.plot==FALSE)
+  {
+  for (i in 1:cat)
+     {
+     x11()
+     plot(model[[i]], title=FALSE)
+     mtext(paste("Category", i, "Model"), side=3, line=1)
+     }
+  }
+}
